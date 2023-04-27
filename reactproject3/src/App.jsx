c1b229c71e785cfd56f3343e86d9658a44b376f9
JaeTriesGit <boffering@hotmail.com>
2023-04-27 12:39:26 +0300

    first commit

---
+++ reactproject3/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [messages, setMessages] = useState(["a","b"]) //This the array that controls the whole thing yeahhhhhh
+
+  return (
+    <div>
+      {
+        messages.length === 0 ? <h1>You're all caught up!</h1> : <h1>You have {messages.length} unread {messages.length > 1 ? "messages" : "message"}</h1> //
+      }
+    </div>
+  )
+}
+
+export default App
